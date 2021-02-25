2caf5d1a2e8f60497fdb7138bab30fab7d8432e5
kuro <konnylizamaya@gmail.com>
2021-02-25 15:47:46 -0600

    borrando lo que no se ocupa para el crud + firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
